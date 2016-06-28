165ecf55c2bd591a336a3a93b9fbdffa120f78e4
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-28 15:43:13 -0400

    Layout is done and styling @ 80%

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,35 @@ import $ from 'jquery';
 
 var baseURL = "https://randomuser.me/api/"
 
-function getUsers() {
-  $.ajax({
-    url: `${baseURL}`,
-    dataType: 'json',
-    data: {
-      results: "12",
-      inc: "name, location, email, phone, id, picture"
-    }
-  }).then(function(data) {
-    console.log(data);
-  })
-};
+function companyTmpl(data) {
+  var template = `
+		<div class="company-grid">
+			<div class="picture">
+				<img src="${data.picture.large}"
+			</div>
+			<div class="name">
+				<h2>${data.name.first.toUpperCase()} ${data.name.last.toUpperCase()}</h2>
+			</div>
+			<p class="email">${data.email.toUpperCase()}</p>
+			<p class="address">${data.location.street}<br>
+			${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+			<div id="social" class="blurry-text" class="social">${data.id.value}</div>
+		</div>
+	`
+  $('.container').append(template)
+}
 
-getUsers();
+$.ajax({
+  url: `${baseURL}`,
+  dataType: 'json',
+  data: {
+    results: "12",
+    inc: "name, location, email, phone, id, picture",
+    nat: "US"
+  }
+}).then(function(response) {
+  console.log(response);
+  response.results.forEach(function(item) {
+    companyTmpl(item)
+  })
+});
